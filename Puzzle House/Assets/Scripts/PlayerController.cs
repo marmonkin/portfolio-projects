de12c8fe920461eae8506ad64de4dfc6545684b0
Maksim Vlasov <maksiim.vlasov@gmail.com>
2025-06-04 14:12:12 +0300

    remade movement
    player moves in the direction which camera faces
    14.12

--- Puzzle House/Assets/Scripts/PlayerController.cs
+++ Puzzle House/Assets/Scripts/PlayerController.cs
@@ -10,14 +10,28 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cam = FindFirstObjectByType<Camera>().transform;
     }
 
     private void Update()
     {
-        float _horInput = Input.GetAxisRaw("Horizontal") * Speed;
-        float _vertInput = Input.GetAxisRaw("Vertical") * Speed;
+        float _horInput = Input.GetAxisRaw("Horizontal");
+        float _vertInput = Input.GetAxisRaw("Vertical");
 
-        rb.linearVelocity = new Vector3(_horInput, rb.linearVelocity.y , _vertInput);
+        Vector3 _camForward = cam.forward;
+        Vector3 _camRight = cam.right;
+
+        _camForward.y = 0;
+        _camRight.y = 0;
+
+        Vector3 _forwardRelative = _vertInput * _camForward;
+        Vector3 _rightRelative = _horInput * _camRight;
+
+        Vector3 _moveDir = _forwardRelative + _rightRelative;
+
+
+        rb.linearVelocity = (_moveDir * Time.deltaTime).normalized * Speed;
+        //rb.linearVelocity = new Vector3(_moveDir.x, rb.linearVelocity.y , _moveDir.z).normalized;
 
         transform.forward = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
     }
