468912230556c1f225d19f4273e8cbeb0848d322
Maksim Vlasov <maksiim.vlasov@gmail.com>
2025-08-21 19:25:38 +0300

    finished with tone functionality
    TODO: make other stuff not interactable when note is up

--- Puzzle House/Assets/Scripts/Interactables/NoteBase.cs
+++ Puzzle House/Assets/Scripts/Interactables/NoteBase.cs
@@ -1,9 +1,76 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NoteBase : MonoBehaviour, IInteractable
 {
+    [Header("Input")]
+    [SerializeField] private KeyCode closeKey;
+
+    [Space(10)]
+    [Header("UI")]
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private TMP_Text canvasText;
+    [SerializeField] private Image canvasImage;
+
+
+    [Space(10)]
+    [Header("Note Parameters")]
+    [SerializeField] [TextArea] private string noteText;
+    [SerializeField] private Image noteImage;
+
+    private bool isOpen = false;
+    private void OnEnable()
+    {
+        if (canvas == null)
+        {
+            canvas = FindAnyObjectByType<Canvas>().gameObject;
+        }
+
+        if (canvasImage == null)
+        {
+            canvasImage = canvas.GetComponentInChildren<Image>();
+        }
+
+        if(canvasText == null)
+        {
+            canvasText = canvasImage.GetComponentInChildren<TMP_Text>();
+        }
+
+    }
+
     public void OnClickAction()
     {
-        Debug.Log("Jarvis, jork it a little");
+        if (!isOpen)
+            ShowNote();
+    }
+
+    private void ShowNote()
+    {
+        canvasText.text = noteText;
+        canvasImage = noteImage;
+
+        canvas.SetActive(true);
+        isOpen = true;
+    }
+
+    private void CloseNote()
+    {
+        canvas.SetActive(false);
+        canvasText.text = null;
+        canvasImage = null;
+        isOpen = false;
+    }
+
+    private void Update()
+    {
+        if (isOpen)
+        {
+            if(Input.GetKeyUp(closeKey))
+            {
+                CloseNote();
+            }
+        }
     }
 }
